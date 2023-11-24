7e4965866202e7003ca8aa0f0074d7badcf1a14a
Fyodor Sologub <141565918+FyodorSologub@users.noreply.github.com>
2023-11-24 22:36:39 +0300

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,72 @@
-import Image from '@/public/chess_pieces/black/pawn.svg'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="">
-      <Image />
+    <main className="grid grid-cols-8 grid-rows-8">
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
+        <div className='w-16 h-16 border bg-slate-600'></div>
+        <div className='w-16 h-16 border bg-slate-400'></div>
     </main>
   );
 };
